fe06b9af06d5fe8d23654ae4dd3cd95b17af73a7
Hanyue He <hanyueh@nvidia.com>
2025-06-09 18:43:21 +0800

    add args

--- examples/offline_inference/flex_kv.py
+++ examples/offline_inference/flex_kv.py
@@ -8,6 +8,7 @@ Learn more about LMCache in https://github.com/LMCache/LMCache.
 """
 import os
 import time
+import argparse
 
 from vllm import LLM, SamplingParams
 from vllm.config import KVTransferConfig
@@ -16,29 +17,23 @@ if __name__ == "__main__":
     import multiprocessing as mp
     mp.set_start_method('spawn', force=True)
 
-    # This example script runs two requests with a shared prefix.
-    shared_prompt_1k = "You are a helpful assistant. " * 160
+    # 接收参数：--seq-len 1000 --cache-ratio 0.5
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seq-len", type=int, default=1000)
+    parser.add_argument("--cache-ratio", type=float, default=1.0)
+    args = parser.parse_args()
+
+
+    base_prompt = "You are a helpful assistant. "  # 6 tokens
+    cached_prompt = int(args.seq_len * args.cache_ratio) // 6 * base_prompt
+    prefill_prompt = (args.seq_len // 6) * base_prompt
     prompts = [
-        # request 0
-        [shared_prompt_1k],
+        # request 0 for warmup
+        [cached_prompt],
         # request 1
-        [shared_prompt_1k + "How are you? ", 
-         shared_prompt_1k + "Who are you? "],
-        # request 2
-        [shared_prompt_1k * 2],
-        # request 3
-        [shared_prompt_1k * 2 + "Tell me a very long story", 
-         shared_prompt_1k * 2 + "Tell me a very short story"],
-        # request 4
-        [shared_prompt_1k * 3],
-        # request 5
-        [shared_prompt_1k * 3 + "What is the capital of France?", 
-         shared_prompt_1k * 3 + "What is the capital of China?",
-         shared_prompt_1k * 3 + "How to make a cake?", 
-         shared_prompt_1k * 3 + "How to make a pizza"]
+        [prefill_prompt + "What is the capital of China? "],
     ]
     
-
     sampling_params = SamplingParams(temperature=0, top_p=0.95, max_tokens=10)
 
     ktc = KVTransferConfig.from_cli(
@@ -46,14 +41,14 @@ if __name__ == "__main__":
     # Set GPU memory utilization to 0.8 for an A40 GPU with 40GB
     # memory. Reduce the value if your GPU has less memory.
     # Note that LMCache is not compatible with chunked prefill for now.
-    llm = LLM(model="Qwen/Qwen3-8B",
+    llm = LLM(model="Qwen/Qwen3-32B",
             kv_transfer_config=ktc,
-            max_model_len=10000,
+            max_model_len=20000,
             enable_chunked_prefill=False,
             enforce_eager=True,
             block_size=16,
             enable_prefix_caching=False,
-            gpu_memory_utilization=0.8)
+            gpu_memory_utilization=0.5)
     ttft_list = []
     seq_len_list = []
     for i, prompt in enumerate(prompts):
@@ -67,6 +62,7 @@ if __name__ == "__main__":
             seq_len_list[i].append(len(output.prompt_token_ids))
             print(f"Generated text: {generated_text!r}")
         print(f"Request {i} done.")
+    print(f"Cache ratio: {args.cache_ratio}")
     for i in range(len(ttft_list)):
         for j in range(len(ttft_list[i])):
             print(f"Request {i} Sequence {j} Length {seq_len_list[i][j]/1000:.3f}k TTFT: {ttft_list[i][j]:3f}s")
